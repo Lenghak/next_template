d8852f85d2ef85562ad5ee1c9570becfa1f4ed1b
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-23 21:20:01 +0700

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,41 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  singleAttributePerLine: true,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  parser: "typescript",
+  plugins: [
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  importOrder: [
+    "^@/scss/(.*)$",
+    "^react(.*)$",
+    "^next/(.*)$",
+    "@^/layouts/(.*)$",
+    "^@/components/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/services/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
+    "^@/types/(.*)$",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  overrides: [
+    {
+      files: "*.scss",
+      options: { parser: "scss" },
+    },
+    {
+      files: "*.json",
+      options: { parser: "json" },
+    },
+  ],
+};
