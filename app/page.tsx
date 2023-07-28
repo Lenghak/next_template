fb2eb5818066581bd67c219ff92ee979f139ce8d
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-28 23:04:56 +0700

    [com\ui] ~ added comps

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      {/* <Button>This is a button</Button> */}
+    </main>
   );
 }
